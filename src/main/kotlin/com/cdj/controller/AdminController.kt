e2aee987b050942c6b8e69851572dd921e0a2ced
niuniucpp <17748408109@163.com>
2024-03-11 20:33:55 +0800

    none

--- src/main/kotlin/com/cdj/controller/AdminController.kt
+++ src/main/kotlin/com/cdj/controller/AdminController.kt
@@ -1,5 +1,7 @@
 package com.cdj.controller
 
+import com.cdj.model.Admin
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -8,8 +10,17 @@ class AdminController {
 
     @RequestMapping("/hello")
     fun hello(): String{
-        val tip: String = "Hello Kotlin And SpringBoot";
+        val tip: String = "Hello Kotlin And SpringBoot"
         return tip
     }
 
+    @GetMapping("/admininfo")
+    fun admininfo(): List<Admin>{
+        var admins = ArrayList<Admin>()
+        var name1 = Admin(1,"NIUNIU","admin")
+        var name2 = Admin(2,"admin","admin")
+        admins.add(name1)
+        admins.add(name2)
+        return admins
+    }
 }
